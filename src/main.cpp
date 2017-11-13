0cf4c23fa7488e1b15e37b997adefe7392575908
Sam <fish23slap@gmail.com>
2017-11-12 19:14:39 -0700

    Added header

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
+#include "more.h"
 
 int main() {
-  std::cout << "Hello, world!" << std::endl;
+  greek();
 }
---
+++ src/more.h
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void greek() {
+  std::cout << "Hello, world!" << std::endl;
+}
